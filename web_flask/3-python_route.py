4def765a94ef553c54b4d18274a71a17101d8468
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-24 03:48:28 +0200

    0x04. AirBnB clone - Web framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,5 +24,13 @@ def c_text(text):
     return 'C {}'.format(text)
 
 
+@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    '''Function returns Python + text statement'''
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
