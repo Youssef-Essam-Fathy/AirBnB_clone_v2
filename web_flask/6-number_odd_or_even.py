6e23010efae12e52a728efd3f65c2f29158375ad
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-24 07:26:12 +0200

    0x04. AirBnB clone - Web framework

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+''' a script to start the Flask web application'''
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Function returns hello statement'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Function returns HBNB statement'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    '''Function returns C + text statement'''
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    '''Function returns Python + text statement'''
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    '''Function returns number statement'''
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    '''Function returns number template'''
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    '''Function returns odd or even template'''
+    if n % 2:
+        state = "odd"
+        msg = "Number: {} is {}".format(n, state)
+        return render_template('6-number_odd_or_even.html', msg=msg)
+    else:
+        state = "even"
+        msg = "Number: {} is {}".format(n, state)
+        return render_template('6-number_odd_or_even.html', msg=msg)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
